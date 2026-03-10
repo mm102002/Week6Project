a1d127956de45670b6634db9ef0f5ddf52669b0e
Mustafa <mustafamasoudi54@gmail.com>
2026-03-09 21:14:05 -0700

    Second commit

--- main/java/org/example/Task2.java
+++ main/java/org/example/Task2.java
@@ -3,7 +3,7 @@ package org.example;
 public class Task2 {
     public static void main(String[] args) {
         int[] values={5, 6, 10, 20, 15};
-        int sum=0;
+        double sum=0;
         for (int i=0; i< values.length; i++){
             sum+=values[i];
         }
